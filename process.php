6d8063abb374d1e8c9c4934d9725ad37e3712022
Basco Creative <adeseghacyril@gmail.com>
2023-08-27 18:04:46 +0100

    hello

--- process.php
+++ process.php
@@ -9,14 +9,14 @@ if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone_number
   $headers = "From: ".$name."<".$email.">";
   $body = "Name: ".$name."\nEmail: ".$email."\nPhone Number: ".$phone_number."\nDepartment & Course: ".$msg_subject."\n\nMessage: ".$message;
   if(mail($to, $msg_subject, $body, $headers)){
-    echo "<div class='alert alert-success'>Thank you for contacting us. We will get back to you as soon as possible.<br><br><a href='index.html'><i class='fa fa-home'></i> Return Home</a></div>";
+    echo "<div class='alert alert-success'>Thank you for contacting us. We will get back to you as soon as possible.<br><br><a href='index'><i class='fa fa-home'></i> Return Home</a></div>";
   }
   else{
-    echo "<div class='alert alert-success'>There was a problem sending your message. Please try again.<br><br><a href='index.html'><i class='fa fa-home'></i> Return Home</a></div>";
+    echo "<div class='alert alert-success'>There was a problem sending your message. Please try again.<br><br><a href='index'><i class='fa fa-home'></i> Return Home</a></div>";
   }
 }
 else{
-  echo "<div class='alert alert-success'>Please fill out all fields before submitting the form.<br><br><a href='index.html'><i class='fa fa-home'></i> Return Home</a></div>";
+  echo "<div class='alert alert-success'>Please fill out all fields before submitting the form.<br><br><a href='index'><i class='fa fa-home'></i> Return Home</a></div>";
 }
 ?>
 
